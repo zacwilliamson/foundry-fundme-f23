62e21baf128c79664571633b8af9064cfce365cd
supersakana <zacwilliamson@icloud.com>
2025-01-30 19:43:34 -0600

    refactored contract code so price feed address belongs in one location and not hardcoded across the tests, scripts, and src files

---
+++ script/HelperConfig.sol
@@ -0,0 +1,4 @@
+// Deploy mocks when we are on a local anvil chain
+// Keep track of contrct address across different chains
+// Sepolia ETH/USD
+// Mainnet ETH/USD
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public view {
@@ -21,7 +23,6 @@ contract FundMeTest is Test {
 
     function testPriceFeedVersionIsAccurate() public view {
         uint256 version = fundMe.getVersion();
-        console.log(version);
         assertEq(version, 4);
     }
     // To test with on for with alchemy seoplia rpc
