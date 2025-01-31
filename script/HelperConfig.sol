dcb2a9b47170ba8078015e35ef7db1aa5e81d963
supersakana <zacwilliamson@icloud.com>
2025-01-30 20:12:14 -0600

    added HelperConfig file to help with getting price feed across different network/environment types

--- script/HelperConfig.sol
+++ script/HelperConfig.sol
@@ -1,4 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+
 // Deploy mocks when we are on a local anvil chain
 // Keep track of contrct address across different chains
 // Sepolia ETH/USD
 // Mainnet ETH/USD
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig {
+    // If we are on a local anvil , we deploy mocks
+    // Otherwise, grab the existing address from the live network
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    // we use the memory keyword because this is a special object that we need stored in memory
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+        NetworkConfig memory seploiaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return seploiaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return ethConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+    }
+}
