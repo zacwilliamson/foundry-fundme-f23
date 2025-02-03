f26cc840c0a2cb4726e26d9f83d06808f61ec8f8
supersakana <zacwilliamson@icloud.com>
2025-02-03 13:34:29 -0600

    added more tests for fundme coverage

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -10,12 +10,13 @@ error FundMe__NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    mapping(address => uint256) public addressToAmountFunded;
-    address[] public funders;
+    mapping(address => uint256) private s_addressToAmountFunded;
+    address[] private s_funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
     address public /* immutable */ i_owner;
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+    // _s is for storage variables
     AggregatorV3Interface private s_priceFeed;
 
     constructor(address priceFeed) {
@@ -26,8 +27,8 @@ contract FundMe {
     function fund() public payable {
         require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
     }
 
     function getVersion() public view returns (uint256) {
@@ -41,11 +42,11 @@ contract FundMe {
     }
 
     function withdraw() public onlyOwner {
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+        for (uint256 funderIndex = 0; funderIndex < s_funders.length; funderIndex++) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
+        s_funders = new address[](0);
         // // transfer
         // payable(msg.sender).transfer(address(this).balance);
 
@@ -76,6 +77,19 @@ contract FundMe {
     receive() external payable {
         fund();
     }
+
+    //  View / Pure functions (Getters)
+    function getAddressToAmountFunded(address fundedAddress) public view returns(uint256){
+        return s_addressToAmountFunded[fundedAddress];
+    }
+
+    function getFunder(uint256 index) public view returns(address){
+        return s_funders[index];
+    }
+
+    function getOwner() external view returns(address){
+        return i_owner;
+    }   
 }
 
 // Concepts we didn't cover yet (will cover in later sections)
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -7,10 +7,14 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
 
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public view {
@@ -18,15 +22,92 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.i_owner(), msg.sender);
+        assertEq(fundMe.getOwner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
     }
+
     // To test with on for with alchemy seoplia rpc
     // forge test --mt testPriceFeedVersionIsAccurate -vvvv --fork-url https://eth-sepolia.g.alchemy.com/v2/IAjtKZ13H-AyHifmaG_w0UgW3YR2RZBG
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructures() public {
+        fundMe.fund{value: SEND_VALUE}();
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+
+    function testFundUpdatesFundedDataStructure() public funded {
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public funded {
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    function testWithdrawFromASingleFunder() public funded {
+        // Arrange
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        // Act
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        // Assert
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+    }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        // if you want to generate numbers for addresses, they have to be uint160
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 2;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            // hoax comes in forges standart lib and is a combination of prank/deal
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+
+            uint256 startingOwnerBalance = fundMe.getOwner().balance;
+            uint256 startingFundMeBalance = address(fundMe).balance;
+
+            vm.startPrank(fundMe.getOwner());
+            fundMe.withdraw();
+            vm.stopPrank();
+
+            assertEq(address(fundMe).balance, 0);
+            assertEq(
+                startingFundMeBalance + startingOwnerBalance,
+                fundMe.getOwner().balance
+            );
+        }
+    }
 }
 
 // forge coverage --fork-url https://eth-sepolia.g.alchemy.com/v2/IAjtKZ13H-AyHifmaG_w0UgW3YR2RZBG
