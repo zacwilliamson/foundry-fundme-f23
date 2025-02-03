05d68cc2957f9f87c5529f645c5682eca74b1fce
supersakana <zacwilliamson@icloud.com>
2025-02-03 14:55:28 -0600

    installed DevOpsTools and set up interactions script

---
+++ script/Interactions.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Fund
+// Withdraw
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract Interactions is Script {
+    uint256 constant SEND_VALUE = 0.01 ether;
+
+    function funFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded FundMe contract with", SEND_VALUE);
+    }
+
+    function run external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        fundFundMe(mostRecentlyDeployed);
+    }
+}
